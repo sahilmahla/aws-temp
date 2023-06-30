d7c35cc514af4eb22926f93de2aa514c3ba47f35
Sahil Mahla <74190139+sahilmahla@users.noreply.github.com>
2023-06-30 14:20:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const dbName = 'MyDBInstance2';
+
 // Create MySQL connection
 const connection = mysql.createConnection({
   host: process.env.RDS_ENDPOINT,
   user: 'admin',
   password: process.env.RDS_PASSWORD,
-  database: dbName
+  database: dbName,
 });
 
 // Connect to MySQL
@@ -23,23 +24,39 @@ connection.connect((err) => {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Create the table if it doesn't exist
+connection.query(
+  `CREATE TABLE IF NOT EXISTS information (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    info VARCHAR(255)
+  )`,
+  (error, results) => {
+    if (error) {
+      console.error('Error creating table:', error);
+    } else {
+      console.log('Table created successfully');
+    }
+  }
+);
+
 // Handle POST request to save data
 app.post('/saveData', (req, res) => {
   const name = req.body.name;
   const id = req.body.id;
 
-  const sql = 'INSERT INTO your_table_name (name, id) VALUES (?, ?)';
+  const sql = 'INSERT INTO information (name, info) VALUES (?, ?)';
   connection.query(sql, [name, id], (err) => {
     if (err) throw err;
     res.send('Data saved successfully!');
   });
 });
 
-// Handle POST request to search data
+// Handle POST request to search data by id and retrieve name
 app.post('/searchData', (req, res) => {
   const searchId = req.body.searchId;
 
-  const sql = 'SELECT name FROM your_table_name WHERE id = ?';
+  const sql = 'SELECT name FROM information WHERE info = ?';
   connection.query(sql, [searchId], (err, results) => {
     if (err) throw err;
 
