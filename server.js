cd3b49cb25a7ec9de24f63ac4b62e9a1373c49e8
Sahil Mahla <74190139+sahilmahla@users.noreply.github.com>
2023-07-03 13:13:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 const app = express();
 const port = 3000;
 const dbName = 'MyDBInstance2';
