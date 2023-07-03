bd52addcd845b2b659e1bf42cd3217891776a3d3
Sahil Mahla <74190139+sahilmahla@users.noreply.github.com>
2023-07-03 13:13:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,6 +67,9 @@ app.post('/searchData', (req, res) => {
     }
   });
 });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // Start the server
 app.listen(port, () => {
